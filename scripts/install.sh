c55cbf85e3ee2de6bb233968c52f86207c17a731
Thejas <thejasa02210@gmail.com>
2026-03-11 22:39:36 +0530

    Change waybar config

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,6 @@
 sudo pacman -Syu
 sudo pacman -S nautilus rofi anki hyprpicker wl-clipboard code telegram-desktop discord waybar hyprpaper hyprpolkitagent hypridle starship wlogout fastfetch grim
 
-sudo rm -r ~/.config/hypr
-sudo rm -r ~/.config/kitty
-sudo rm -r ~/.config/fastfetch
-sudo rm -r ~/.config/rofi
-sudo rm -r ~/.config/wlogout
 sudo rm ~/.config/starship.toml
 
 ln -sfn ~/dotfiles/hyprland ~/.config/hypr
@@ -14,7 +9,7 @@ ln -sfn ~/dotfiles/kitty ~/.config/kitty
 ln -sfn ~/dotfiles/fastfetch ~/.config/fastfetch
 ln -sfn ~/dotfiles/rofi ~/.config/rofi
 ln -sfn ~/dotfiles/wlogout ~/.config/wlogout
-
+ln -sfn ~/dotfiles/waybar/ ~/.config/waybar
 
 source ~/.config/fish/config.fish
 
