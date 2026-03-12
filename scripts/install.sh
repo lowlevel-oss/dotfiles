fbf70c6d52f3bae4cf19305107fffd666633e76e
Thejas <thejasa02210@gmail.com>
2026-03-12 08:14:26 +0530

    Update

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 sudo pacman -Syu
-sudo pacman -S nautilus rofi anki hyprpicker wl-clipboard code telegram-desktop discord waybar hyprpaper hyprpolkitagent hypridle starship wlogout fastfetch grim mako
+sudo pacman -S nautilus rofi anki hyprpicker wl-clipboard code telegram-desktop discord waybar hyprpaper hyprpolkitagent hypridle starship wlogout fastfetch grim mako hyprlock ttf-meslo-nerd-font-powerlevel10k ttf-jetbrains-mono-nerd
 
 sudo rm ~/.config/starship.toml
 
@@ -11,6 +11,17 @@ ln -sfn ~/dotfiles/rofi ~/.config/rofi
 ln -sfn ~/dotfiles/wlogout ~/.config/wlogout
 ln -sfn ~/dotfiles/waybar/ ~/.config/waybar
 
-source ~/.config/fish/config.fish
+fish -c "source ~/.config/fish/config.fish"
 
-set -U fish_greeting "Welcome back ⚡"
+fish -c 'set -U fish_greeting "Welcome back ⚡"'
+
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/keyitdev/sddm-astronaut-theme/master/setup.sh)"
+
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru || exit 1
+makepkg -si
+
+cd ~
+
+paru -S catppuccin-gtk-theme-mocha
